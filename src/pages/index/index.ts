333049b442c3b5fd0b644fc101a49bea12167dc5
c1DeepInside <vanya0759@gmail.com>
2022-12-18 18:51:13 +0300

    refactor: rename filter.ts

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -2,7 +2,7 @@ import './styles/style.scss';
 import { products } from "../../data/products";
 import { renderGoods } from './scripts/render';
 import { Product } from '../../data/interfaces';
-import { minMax } from './scripts/filter';
+import { minMax } from './scripts/filter-price';
 
 renderGoods(products);
 
