1b09cd4a919378c3dbf183a8b8b32ed07d45d885
c1DeepInside <vanya0759@gmail.com>
2022-12-19 20:07:34 +0300

    feat: style sort select

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -3,7 +3,9 @@ import { renderGoods } from './scripts/render';
 import { products } from "../../data/products";
 import { renderFilters } from './scripts/renderFilters';
 import { showFilters } from './scripts/showFilters';
+import { sort } from './scripts/sort';
 
+sort();
 renderGoods(products);
 showFilters(products);
 renderFilters({ fromSilderId: '#fromInput', toSliderId: '#toInput', fromValueId: '#from-Slider', toValueId: '#to-Slider' });
