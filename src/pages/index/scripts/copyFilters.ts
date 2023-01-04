f9cc3ea63659b6a0f9d89218895195a0d0715ca5
Romanova Tanya <heavens1xdoor@mail.ru>
2023-01-04 15:58:53 +0300

    fix: change innerHtml to innerText

--- src/pages/index/scripts/copyFilters.ts
+++ src/pages/index/scripts/copyFilters.ts
@@ -1,6 +1,5 @@
 export function copyFilters() {
   const btn: HTMLButtonElement = document.querySelector('.filters__btn')!;
-
   btn.addEventListener('click', (): void => {
     btn.innerText = 'Filters copied!';
     btn.disabled = true;
@@ -9,7 +8,7 @@ export function copyFilters() {
       btn.disabled = false;
     }, 1000);
     if(navigator.clipboard) {
-      navigator.clipboard.writeText(window.location.href).then();
+      navigator.clipboard.writeText(window.location.href);
     } else {
       console.log('Browser Not compatible');
     }
